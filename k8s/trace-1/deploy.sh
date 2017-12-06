bec0401657753904bf97f6250ab3383ebefd780c
jnsone11 <jens.neuse@gmx.de>
2017-12-06 08:39:43 +0100

    parallel backend processing

--- k8s/trace-1/deploy.sh
+++ k8s/trace-1/deploy.sh
@@ -3,15 +3,15 @@
 namespace="trace-1"
 
 istioctl kube-inject -f database-deployment.yaml | kubectl -n $namespace apply -f -
-database-service.yaml
+kubectl -n $namespace apply -f database-service.yaml
 istioctl kube-inject -f horrorskope-api-deployment.yaml | kubectl -n $namespace apply -f -
-horrorskope-api-service.yaml
+kubectl -n $namespace apply -f horrorskope-api-service.yaml
 istioctl kube-inject -f horrorskope-external-api-deployment.yaml | kubectl -n $namespace apply -f -
-horrorskope-external-api-service.yaml
+kubectl -n $namespace apply -f horrorskope-external-api-service.yaml
 istioctl kube-inject -f renderine-deployment.yaml | kubectl -n $namespace apply -f -
-renderine-ingress.yaml
-renderine-service.yaml
+kubectl -n $namespace apply -f renderine-ingress.yaml
+kubectl -n $namespace apply -f renderine-service.yaml
 istioctl kube-inject -f weather-api-deployment.yaml | kubectl -n $namespace apply -f -
-weather-api-service.yaml
+kubectl -n $namespace apply -f weather-api-service.yaml
 istioctl kube-inject -f weather-external-api-deployment.yaml | kubectl -n $namespace apply -f -
-weather-external-api-service.yaml
+kubectl -n $namespace apply -f weather-external-api-service.yaml

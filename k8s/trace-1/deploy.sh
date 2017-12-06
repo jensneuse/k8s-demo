43f6730e7b1f4b5f9e5ea357acc52970bcfc29da
jnsone11 <jens.neuse@gmx.de>
2017-12-06 08:35:01 +0100

    parallel backend processing

--- k8s/trace-1/deploy.sh
+++ k8s/trace-1/deploy.sh
@@ -2,8 +2,16 @@
 
 namespace="trace-1"
 
-for file in Data/*.yaml
-do
-    name=${file##*/}
-    istioctl kube-inject -f $name | kubectl -n $namespace apply -f -
-done
+kubectl -n $namespace apply -f <(istioctl kube-inject -f database-deployment.yaml)
+database-service.yaml
+kubectl -n $namespace apply -f <(istioctl kube-inject -f horrorskope-api-deployment.yaml)
+horrorskope-api-service.yaml
+kubectl -n $namespace apply -f <(istioctl kube-inject -f horrorskope-external-api-deployment.yaml)
+horrorskope-external-api-service.yaml
+kubectl -n $namespace apply -f <(istioctl kube-inject -f renderine-deployment.yaml)
+renderine-ingress.yaml
+renderine-service.yaml
+kubectl -n $namespace apply -f <(istioctl kube-inject -f weather-api-deployment.yaml)
+weather-api-service.yaml
+kubectl -n $namespace apply -f <(istioctl kube-inject -f weather-external-api-deployment.yaml)
+weather-external-api-service.yaml

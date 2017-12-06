3e0d9fa1ee5fd6f6a64291394dac37b7d31816d5
jnsone11 <jens.neuse@gmx.de>
2017-12-06 08:26:37 +0100

    parallel backend processing

--- k8s/trace-1/deploy.sh
+++ k8s/trace-1/deploy.sh
@@ -5,5 +5,5 @@ namespace="trace-1"
 for file in Data/*.yaml
 do
     name=${file##*/}
-    kubectl -n $namespace apply -f <(istioctl kube-inject -f $name)
+    istioctl kube-inject -f $name | kubectl -n $namespace apply -f -
 done
